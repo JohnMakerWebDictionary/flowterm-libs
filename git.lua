04c03e2a2a440547b0b05689278f6677791ed2a8
JohnMakerWebDictionary <diopelhijja@gmail.com>
2025-04-07 01:49:46 +0000

    Update git.lua

--- git.lua
+++ git.lua
@@ -1,1 +1,18 @@
 
+-- git.lua
+local git = {}
+
+-- Clone a repository
+function git.clone(repo_url)
+    os.execute("git clone " .. repo_url)
+end
+
+-- Check Git version
+function git.version()
+    local handle = io.popen("git --version 2>&1")
+    local result = handle:read("*a")
+    handle:close()
+    return result
+end
+
+return git  -- Must return a table
